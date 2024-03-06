5ef1516cd7cf1aa87fe5e6e7836021c42623d550
Maria Carolina <demycar24@gmail.com>
2024-03-06 19:42:52 -0300

    Cartoon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <>
+    <h1>aaaaaaaaaa!</h1>
+    <Header/>
+    </>
+  )
+}
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./HeaderStyle"
+//importe (*)tudo como S de "./caminho"
+
+function Header() {
+    return (
+        <S.StyleHeader>
+            <h1>teste</h1>
+        </S.StyleHeader>
+    )
+}
+export default Header
